1c75dcf2ff04f94fc5e92133c1832e8bb0d9ab04
Selftishz <art2sys048@gmail.com>
2024-03-26 23:40:00 +0300

    Kafka has been added

--- src/main/java/com/example/kafkademo/kafka/KafkaConsumer.java
+++ src/main/java/com/example/kafkademo/kafka/KafkaConsumer.java
@@ -1,2 +1,19 @@
-package kafka;public class KafkaConsumer {
+package com.example.kafkademo.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class.getName());
+
+    @KafkaListener(topics = "course", groupId = "my_consumer")
+    public void listen(String message) {
+        log.info("Received message: " + message);
+    }
 }
